80a3d70226ff5d68caed5c32cd39edde9572d0fe
John Bedworth <john@vertiso.com>
2025-04-09 19:49:46 -0700

    added neotest to nvim

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,9 @@
+return {
+	"nvim-neotest/neotest",
+	depenendencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"autoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+}
